de54a610a7ad7d1288c453e0b783efe177e9f394
Ben Stern <bstern@us.ibm.com>
2020-06-22 11:50:37 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -74,3 +74,42 @@ variable "cpus" {
 variable "memory" {
   default="8192"
 }
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "http2"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 4
+  memory   = 8192
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  cdrom {
+    client_device = true
+  }
+
+  vapp {
+    properties {
+      user-data = "${base64encode(file("cloud-init.yml"))}"
+    }
+  }
+
+  clone {
+#    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+  }
+}
